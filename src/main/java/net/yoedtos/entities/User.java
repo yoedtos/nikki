7c5e4de7d8d0958ab185f397c1d366e8d524c3e6
Edson Yoshimaru <yoedtos@hotmail.com>
2023-04-01 10:52:18 +0900

    feat: added user object

---
+++ src/main/java/net/yoedtos/entities/User.java
@@ -0,0 +1,21 @@
+package net.yoedtos.entities;
+
+import io.vavr.control.Either;
+import net.yoedtos.entities.error.InvalidEmailError;
+
+public class User {
+    public final Email email;
+
+    private User(Email email) {
+        this.email = email;
+    }
+
+    public static Either<InvalidEmailError, User> create(UserData userData) {
+        var emailOrError = Email.create(userData.getEmail());
+        if(emailOrError.isLeft()) {
+            return Either.left(new InvalidEmailError());
+        }
+        var email = emailOrError.get();
+        return Either.right(new User(email));
+    }
+}
---
+++ src/main/java/net/yoedtos/entities/UserData.java
@@ -0,0 +1,8 @@
+package net.yoedtos.entities;
+
+import lombok.Value;
+
+@Value
+public class UserData {
+    private String email;
+}
