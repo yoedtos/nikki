d1654e1e57f3f667badc9c7b78672c8d25527ac7
Edson Yoshimaru <yoedtos@hotmail.com>
2023-04-06 12:19:23 +0900

    feat: added password to user

--- src/main/java/net/yoedtos/entities/User.java
+++ src/main/java/net/yoedtos/entities/User.java
@@ -2,20 +2,28 @@ package net.yoedtos.entities;
 
 import io.vavr.control.Either;
 import net.yoedtos.entities.error.InvalidEmailError;
+import net.yoedtos.entities.error.InvalidPasswordError;
 
 public class User {
     public final Email email;
+    public final Password password;
 
-    private User(Email email) {
+    private User(Email email, Password password) {
         this.email = email;
+        this.password = password;
     }
 
-    public static Either<InvalidEmailError, User> create(UserData userData) {
+    public static Either<Error, User> create(UserData userData) {
         var emailOrError = Email.create(userData.getEmail());
         if(emailOrError.isLeft()) {
             return Either.left(new InvalidEmailError(userData.getEmail()));
         }
+        var passwordOrError = Password.create(userData.getPassword());
+        if(passwordOrError.isLeft()) {
+            return Either.left(new InvalidPasswordError());
+        }
         var email = emailOrError.get();
-        return Either.right(new User(email));
+        var password = passwordOrError.get();
+        return Either.right(new User(email, password));
     }
 }
