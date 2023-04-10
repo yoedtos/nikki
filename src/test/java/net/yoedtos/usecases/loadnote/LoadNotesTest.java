070a2285c3c59bb41b3e5801446a868ef82f301b
Edson Yoshimaru <yoedtos@hotmail.com>
2023-04-10 22:39:28 +0900

    test: load note use case

---
+++ src/test/java/net/yoedtos/usecases/loadnote/LoadNotesTest.java
@@ -0,0 +1,38 @@
+package net.yoedtos.usecases.loadnote;
+
+import static net.yoedtos.usecases.TestConstant.VALID_EMAIL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import net.yoedtos.usecases.doubles.repositories.InMemoryNoteRepository;
+import net.yoedtos.usecases.createnote.ports.NoteData;
+import net.yoedtos.usecases.createnote.ports.NoteRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LoadNotesTest {
+    private NoteRepository noteRepository;
+    private Long validUserId = 0L;
+    private final String titleOne = "Note 1";
+    private final String titleTwo = "Note 2";
+
+    @Before
+    public void initObject() {
+        var notesData = new ArrayList<>(Arrays.asList(
+                new NoteData(0L, validUserId, VALID_EMAIL, titleOne, "Content One"),
+                new NoteData(1L, validUserId, VALID_EMAIL, titleTwo, "Content Two")));
+
+        noteRepository = new InMemoryNoteRepository(notesData);
+    }
+
+    @Test
+    public void shouldLoadNotesForRegisteredUser() {
+        var loadNoteUseCase = new LoadNotes(noteRepository);
+        var notes = loadNoteUseCase.perform(validUserId).get();
+        assertThat(notes.size()).isEqualTo(2);
+        assertThat(notes.get(0).getTitle()).isEqualTo(titleOne);
+        assertThat(notes.get(1).getTitle()).isEqualTo(titleTwo);
+    }
+}
