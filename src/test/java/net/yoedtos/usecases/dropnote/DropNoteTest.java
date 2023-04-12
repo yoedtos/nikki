0fc688aefc0c61aa769e8baeecaa5c9af8a91900
Edson Yoshimaru <yoedtos@hotmail.com>
2023-04-12 20:16:46 +0900

    refactor: move constants to class testconstant

--- src/test/java/net/yoedtos/usecases/dropnote/DropNoteTest.java
+++ src/test/java/net/yoedtos/usecases/dropnote/DropNoteTest.java
@@ -1,10 +1,10 @@
 package net.yoedtos.usecases.dropnote;
 
-import static net.yoedtos.usecases.TestConstant.VALID_EMAIL;
+import static net.yoedtos.usecases.TestConstant.NOTE_ID;
+import static net.yoedtos.usecases.TestConstant.NOTE_ONE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import net.yoedtos.usecases.doubles.repositories.InMemoryNoteRepository;
-import net.yoedtos.usecases.ports.NoteData;
 import net.yoedtos.usecases.ports.NoteRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,22 +13,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DropNoteTest {
-    private Long validUserId = 0L;
-    private Long noteId = 0L;
-    private final String titleOne = "Note 1";
-    private NoteData noteOne;
     private NoteRepository noteRepository;
 
     @Before
     public void initObject() {
-        noteOne =  new NoteData(0L, validUserId, VALID_EMAIL, titleOne, "Content One");
-        noteRepository = new InMemoryNoteRepository(new ArrayList<>(List.of(noteOne)));
+        noteRepository = new InMemoryNoteRepository(new ArrayList<>(List.of(NOTE_ONE)));
     }
     @Test
     public void shouldRemoveExistingNote() {
         var dropNoteUseCase = new DropNote(noteRepository);
-        var dropped = dropNoteUseCase.perform(noteId).get();
-        assertThat(dropped).isEqualTo(noteOne);
-        assertThat(noteRepository.findNote(noteId)).isNull();
+        var dropped = dropNoteUseCase.perform(NOTE_ID).get();
+        assertThat(dropped).isEqualTo(NOTE_ONE);
+        assertThat(noteRepository.findNote(NOTE_ID)).isNull();
     }
 }
--- src/test/java/net/yoedtos/usecases/loadnote/LoadNotesTest.java
+++ src/test/java/net/yoedtos/usecases/loadnote/LoadNotesTest.java
@@ -1,10 +1,9 @@
 package net.yoedtos.usecases.loadnote;
 
-import static net.yoedtos.usecases.TestConstant.VALID_EMAIL;
+import static net.yoedtos.usecases.TestConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import net.yoedtos.usecases.doubles.repositories.InMemoryNoteRepository;
-import net.yoedtos.usecases.ports.NoteData;
 import net.yoedtos.usecases.ports.NoteRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,27 +13,21 @@ import java.util.Arrays;
 
 public class LoadNotesTest {
     private NoteRepository noteRepository;
-    private Long validUserId = 0L;
     private Long invalidUserId = 1L;
-    private final String titleOne = "Note 1";
-    private final String titleTwo = "Note 2";
 
     @Before
     public void initObject() {
-        var notesData = new ArrayList<>(Arrays.asList(
-                new NoteData(0L, validUserId, VALID_EMAIL, titleOne, "Content One"),
-                new NoteData(1L, validUserId, VALID_EMAIL, titleTwo, "Content Two")));
-
+        var notesData = new ArrayList<>(Arrays.asList(NOTE_ONE, NOTE_TWO));
         noteRepository = new InMemoryNoteRepository(notesData);
     }
 
     @Test
     public void shouldLoadNotesForRegisteredUser() {
         var loadNoteUseCase = new LoadNotes(noteRepository);
-        var notes = loadNoteUseCase.perform(validUserId).get();
+        var notes = loadNoteUseCase.perform(VALID_USER_ID).get();
         assertThat(notes.size()).isEqualTo(2);
-        assertThat(notes.get(0).getTitle()).isEqualTo(titleOne);
-        assertThat(notes.get(1).getTitle()).isEqualTo(titleTwo);
+        assertThat(notes.get(0).getTitle()).isEqualTo(TITLE_ONE);
+        assertThat(notes.get(1).getTitle()).isEqualTo(TITLE_TWO);
     }
 
     @Test
