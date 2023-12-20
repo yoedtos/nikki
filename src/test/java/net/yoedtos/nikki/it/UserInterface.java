f095a8a45868c1f6fdab602fcc51816c9f67cdbd
Edson Yoshimaru <yoedtos@hotmail.com>
2023-12-20 23:21:20 +0900

    test: note creation issue when drop or view note

--- src/test/java/net/yoedtos/nikki/it/UserInterface.java
+++ src/test/java/net/yoedtos/nikki/it/UserInterface.java
@@ -4,8 +4,7 @@ import static net.yoedtos.nikki.main.config.Constants.ENTER_FXML;
 import static net.yoedtos.nikki.main.config.Constants.MAIN_APP_TITLE;
 import static net.yoedtos.nikki.main.factories.Factory.Operation.SIGNIN;
 import static net.yoedtos.nikki.main.factories.Factory.Operation.SIGNUP;
-import static net.yoedtos.nikki.usecases.TestConstant.VALID_EMAIL;
-import static net.yoedtos.nikki.usecases.TestConstant.VALID_PASSWORD;
+import static net.yoedtos.nikki.usecases.TestConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
@@ -16,6 +15,7 @@ import net.yoedtos.nikki.controllers.SignUpController;
 import net.yoedtos.nikki.external.view.EnterFxController;
 import net.yoedtos.nikki.external.view.FxmlHelper;
 import net.yoedtos.nikki.main.factories.Factory;
+import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.util.WaitForAsyncUtils;
@@ -39,17 +39,46 @@ public class UserInterface extends ApplicationTest {
         enterFxController.onStageDefined(new Stage());
     }
 
-    @Test
-    public void shouldEnterAndNavigateThroughTabs() {
+    @Before
+    public void loginIn() {
         clickOn("#txtEmail").write(VALID_EMAIL);
         clickOn("#pwdPassword").write(VALID_PASSWORD);
         clickOn("#btnApply");
 
         WaitForAsyncUtils.waitForFxEvents();
+    }
 
+    @Test
+    public void shouldEnterAndNavigateThroughTabs() {
         assertThat(targetWindow(MAIN_APP_TITLE)).isNotNull();
         clickOn("#tabCreate").clickOn("#tabList");
         verifyThat(VALID_EMAIL, isVisible());
         clickOn("#tabAbout").clickOn("#tabHome");
+    }
+
+    @Test
+    public void shouldCreateNoteAfterViewNote() {
+        clickOn("#tabList").clickOn(TITLE_ONE);
+
+        clickOn("#tabCreate").clickOn("#txtTitle").write(TITLE_MOD);
+        clickOn("#htmlContent").write(CONTENT_MOD);
+        clickOn("#btnSave");
+
+        clickOn("#tabList");
+        var noteList = lookup("#viewNotes").queryListView().getItems();
+        assertThat(noteList).hasSize(3).asString().contains(TITLE_ONE, TITLE_TWO, TITLE_MOD);
+    }
+
+    @Test
+    public void shouldCreateNoteAfterDropNote() {
+        clickOn("#tabList").clickOn(TITLE_TWO).clickOn("#btnDrop");
+
+        clickOn("#tabCreate").clickOn("#txtTitle").write(TITLE_TWO);
+        clickOn("#htmlContent").write(CONTENT_TWO);
+        clickOn("#btnSave");
+
+        clickOn("#tabList");
+        var noteList = lookup("#viewNotes").queryListView().getItems();
+        assertThat(noteList).hasSize(2).asString().contains(TITLE_ONE, TITLE_TWO);
     }
 }
